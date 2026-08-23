55b450bef2297fb1f72c61611e2e95270bb5b6ba
Localinst <giuseppetotirebusiness@gmail.com>
2026-08-23 13:17:57 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,8 @@ import fs from 'fs';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  server: {
+  base: '/tabooArgiro/',
+server: {
     host: "::",
     port: 8080,
   },
